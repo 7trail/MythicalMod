2aa93b7e6a85ea36679d8a8a41840db1a7bb2551
amber <anothersaiemail@gmail.com>
2022-08-01 15:10:04 -0500

    a

---
+++ StatusBarMod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace Mythical
+{
+    public class StatusBarMod : MonoBehaviour
+    {
+        public PlayerStatusBar self;
+        public void Update()
+        {
+            if (self.playerPortrait != null && ContentLoader.newPalette != null)
+            {
+                Material material = UnityEngine.Object.Instantiate<Material>(self.playerPortrait.material);
+                material.SetFloat("_PaletteCount", 32 + ContentLoader.palettes.Count);
+                material.SetTexture("_Palette", ContentLoader.newPalette);
+                self.playerPortrait.material = material;
+                Destroy(this);
+            }
+        }
+    }
+}
